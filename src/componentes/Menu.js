780892fc2e75c15cd57f3e50fb2152483405a676
Davi André Gauze <115033228+DaviGauze@users.noreply.github.com>
2025-05-13 21:59:31 -0300

    Criação das páginas padrões

--- src/componentes/Menu.js
+++ src/componentes/Menu.js
@@ -3,9 +3,7 @@ export default function Menu() {
         <>
             <nav className="navbar navbar-expand-lg navbar-light bg-primary">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">
-                Navbar
-                </a>
+
                 <button
                 className="navbar-toggler"
                 type="button"
@@ -20,13 +18,13 @@ export default function Menu() {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">
+                    <a className="nav-link active" aria-current="page" href="/">
                         Home
                     </a>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link" href="#">
-                        Link
+                    <a className="nav-link" href="/categorias">
+                        Listar
                     </a>
                     </li>
                     <li className="nav-item dropdown">
@@ -38,17 +36,17 @@ export default function Menu() {
                         data-bs-toggle="dropdown"
                         aria-expanded="false"
                     >
-                        Dropdown
+                        Cadastros
                     </a>
                     <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
                         <li>
-                        <a className="dropdown-item" href="#">
-                            Action
+                        <a className="dropdown-item" href="/categorias">
+                            Listar Categorias
                         </a>
                         </li>
                         <li>
-                        <a className="dropdown-item" href="#">
-                            Another action
+                        <a className="dropdown-item" href="/categoria">
+                            Nova Categoria
                         </a>
                         </li>
                         <li>
@@ -62,27 +60,8 @@ export default function Menu() {
                     </ul>
                     </li>
                     <li className="nav-item">
-                    <a
-                        className="nav-link disabled"
-                        href="#"
-                        tabIndex={-1}
-                        aria-disabled="true"
-                    >
-                        Disabled
-                    </a>
                     </li>
                 </ul>
-                <form className="d-flex">
-                    <input
-                    className="form-control me-2"
-                    type="search"
-                    placeholder="Search"
-                    aria-label="Search"
-                    />
-                    <button className="btn btn-outline-success" type="submit">
-                    Search
-                    </button>
-                </form>
                 </div>
             </div>
             </nav>
